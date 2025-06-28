8c927dd30922009167d2e530d875cc1b925e0550
Blaise Le Coquil <blaiselcq@blaiselcq.net>
2025-06-28 18:43:26 +0200

    2025 holy saturday part 2

--- src/2025/holy-saturday.cpp
+++ src/2025/holy-saturday.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <map>
+#include <optional>
 #include <string>
 #include <vector>
-#include <map>
 
 using Replacements = std::map<std::vector<std::string>, std::vector<std::string>>;
 
@@ -26,16 +27,55 @@ std::pair<Replacements, std::vector<std::string>> parse_input() {
   return std::make_pair(std::move(replacements), std::move(lines[0]));
 }
 
-std::vector<std::string> replace(const Replacements& replacements,
-                                 std::vector<std::string> input) {
+std::optional<std::vector<std::string>> replacement(std::span<const std::string> input,
+                                                    const std::vector<std::string>& from,
+                                                    const std::vector<std::string>& to,
+                                                    int puzzle_number) {
+  const auto it = input.begin();
+
+  if (puzzle_number == 1) {
+    if (from == std::vector(it, std::next(it, from.size())))
+      return std::make_optional(to);
+    return std::nullopt;
+  }
+ 
+  std::array<char, 10> replacements{0};
+  for (auto i = 0; i < from.size(); i++) {
+    if (from[i].size() != (*(it + i)).size()) return std::nullopt;
+    if (from[i][0] != (*(it + i))[0]) return std::nullopt;
+
+    for (auto j = 1; j < from[i].size(); j++) {
+      auto c_from = from[i][j];
+      auto c_input = (*(it + i))[j];
+
+      if (c_input % 2 != c_from % 2) return std::nullopt;
+      if (replacements[c_from - 0x30] != 0) {
+        if (replacements[c_from - 0x30] != c_input) return std::nullopt;
+      }
+      replacements[c_from - 0x30] = c_input;
+    }
+  }
+  auto result = std::vector<std::string>{};
+  for (const auto& to_str: to) {
+    auto& line = result.emplace_back(to_str);
+    for (auto it = line.begin() + 1; it != line.end(); it++)
+      *it = replacements[*it - 0x30];
+  }
+  return std::make_optional(std::move(result));
+} 
+
+std::vector<std::string> replace_puzzle(const Replacements& replacements,
+                                        std::vector<std::string> input,
+                                        int puzzle_number) {
   auto it = input.begin();
   while (it != input.end()) {
     for (const auto& [from, to]: replacements) {
       if (std::distance(it, input.end()) < from.size()) continue;
-      if (from == std::vector(it, std::next(it, from.size()))) {
-        it = input.insert(it, to.begin(), to.end());
-        input.erase(std::next(it, to.size()),
-                    std::next(it, from.size() + to.size()));
+      if (auto rep = replacement({it, input.end()}, from, to, puzzle_number);
+          rep) {
+        it = input.insert(it, rep->begin(), rep->end());
+        input.erase(std::next(it, rep->size()),
+                    std::next(it, from.size() + rep->size()));
         break;
       }
     }
@@ -45,14 +85,20 @@ std::vector<std::string> replace(const Replacements& replacements,
 }
 
 int main(int, char **argv) {
+  const auto puzzle_number = *argv[1] - 0x30;
+
   auto [replacements, input] = parse_input();
 
-  input = replace(replacements, std::move(input));
-
   auto sum = 0;
-  for (const auto& line: input) sum += std::stoi(line);
+  auto psum = 1;
+
+  while (psum != sum) {
+    psum = sum;
+    sum = 0;
+    input = replace_puzzle(replacements, std::move(input), puzzle_number);
+    for (const auto& line: input) sum += std::stoi(line);
+    if (puzzle_number == 1) break;
+  }
 
   std::cout << sum;
-
-  // const auto puzzle_number = *argv[1] - 0x30;
 }
